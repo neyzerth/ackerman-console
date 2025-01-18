431a2e114e99a6f9b8dba0f0b31623384ce5fbef
Neyzer <neyzertoledo@gmail.com>
2025-01-17 20:41:37 -0800

    beta 0.1 :)
    Co-authored-by: carrlosamartin117@gmail.com <carrlosamartin117@gmail.com>

--- Ackerman/Program.cs
+++ Ackerman/Program.cs
@@ -1,89 +1,87 @@
 ﻿class Program 
 {
     static void Main(String[] args)
-    {
-        MainMenu();
-    }
-
-    // ulong es un int de 64 bits (System.Int64)
-    // pero solo toma los valores positivos
-    // valor max aprox 1.8x10^20
-    static ulong Ackerman(int m, int n)
-    {
-        //si m = 0         : n +1
-        //si m > 0 y n = 0 : acker([m-1], 1)
-        //si m > 0 y n > 0 : acker([m-1], acker(m,[m-1]))
-        
-        return 0; //temporal
-    }
-    public static void MainMenu(){
-        bool validacion;
-
+    {   
+        bool bandera;
         do
         {
             Console.Clear();
-            PrintXY(10,1,"Ackermann");
-            try
-            {
-                validacion = Solicitar();
-            }
-            catch (System.Exception)
-            {
-                validacion = false;
-            }
-        } while (!validacion);
+            ImprimirAckerman();
+            bandera = validarContinuacion();
+        } while (bandera);
+
+        Console.WriteLine("\nAdios UwU ");
     }
-    public static bool Solicitar(){
-        int m = 0, n = 0;
-        bool validacion;
+    static void ImprimirAckerman(){
+        int m = ValidarNumero("\tIngrese un numero del 0 al 3 para m: ");
+        int n = ValidarNumero("\tIngrese un numero del 0 al 3 para n: ");
 
-        do
-        {
-            PrintXY(10,3,"                                                                 ");
-            PrintXY(10,3,"Ingrese un número del 0 al 3 para m: ");
-            try
-            {
-                m = Int32.Parse(Console.ReadLine());
-                validacion = ValidarNumero(m);
-            }
-            catch (System.Exception)
-            {
-                validacion = false;
-            }
-        } while (!validacion);
-        //DRY this
-        do
-        {
-            PrintXY(10,4,"                                                                 ");
-            PrintXY(10,4,"Ingrese un número del 0 al 3 para n: ");
-            try
-            {
-                n = Int32.Parse(Console.ReadLine());
-                validacion = ValidarNumero(n);
-            }
-            catch (System.Exception)
-            {
-                validacion = false;
-            }
-        } while (!validacion);
-
-        ImprimirAckermann(m,n);
-
-        return validacion;
-
+        Console.WriteLine($"\n\tAckerman({m},{n}) = " + Ackerman(m, n));
     }
-    public static bool ValidarNumero(int numero){
-        if (numero >= 0 && numero <= 3)
-            return true;
+    static int Ackerman(int m, int n)
+    {
+        //si m = 0 : n +1
+        if (m == 0 ) return n + 1;
+        //si m > 0 y n = 0 : acker([m-1], 1)
+        if(m > 0 && n == 0) return Ackerman((m - 1),1);
+        //si m > 0 y n > 0 : acker( [m-1], acker(m,[n-1]) )
+        return Ackerman((m - 1), Ackerman(m ,(n - 1)));
+    }
 
-        return false;
+    static int ValidarNumero(String texto){
+        int num = 0;
+
+        Console.Write(texto); 
+
+        int posx = Console.CursorLeft;
+        int posy = Console.CursorTop;
+
+        do{
+            PrintXY(posx, posy, "                ");
+            PrintXY(posx, posy, "");
+            
+            try {
+                num = Int32.Parse(Console.ReadLine());
+                
+                //si se ingresa un numero valido, se rompe el ciclo
+                if(num >= 0 && num  <= 3)
+                    break; 
+
+            } catch(Exception){
+                Console.WriteLine("\tFavor de Ingresar Un Numero Valido");
+                continue; //Continua el ciclo
+            }
+            Console.WriteLine("\tFavor de Ingresar Un Numero entre 0 y 3");
+
+        } while (true);
+        
+        PrintXY(0, posy+1, "\t                                              ");
+        PrintXY(0, posy+1, "");
+
+        return num;
     }
-    public static void ImprimirAckermann(int m, int n){
-        PrintXY(10,6,"Ackermann(" + m + "," + n + ") = resultado");
-    }
-    public static void PrintXY(int x, int y, string text){
+    static void PrintXY(int x, int y, string text){
         Console.SetCursorPosition(x,y);
         Console.Write(text);
+    }
+
+    static bool validarContinuacion(){
+        String respuesta;
+        bool validacion;
+
+        int posx = Console.CursorLeft;
+        int posy = Console.CursorTop;
+
+        do{
+            PrintXY(posx, posy, "                ");
+            PrintXY(posx, posy, "");
+            Console.Write("\n\tDesea continuar? (S/n): ");
+            respuesta = Console.ReadLine().ToLower();
+
+            validacion = respuesta.Equals("s") || respuesta.Equals("n");
+        } while (!validacion);
+
+        return respuesta.Equals("s");
     }
 
 }
